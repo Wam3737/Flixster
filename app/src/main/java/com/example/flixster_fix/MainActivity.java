affa3c89ef97e2efb42e57bde75b05444bbaf965
Will <will.matthews1@aol.com>
2021-02-14 13:17:10 -0500

    Movie class created

--- app/src/main/java/com/example/flixster_fix/MainActivity.java
+++ app/src/main/java/com/example/flixster_fix/MainActivity.java
@@ -7,6 +7,13 @@ import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster_fix.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 import okhttp3.Headers;
 
@@ -14,6 +21,7 @@ import okhttp3.Headers;
 public class MainActivity extends AppCompatActivity {
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
+    List<Movie> movies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +32,15 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(int i, Headers headers, JSON json) {
                         Log.d(TAG, "Success");
+                        JSONObject jsonObject = json.jsonObject;
+                        try{
+                            JSONArray results = jsonObject.getJSONArray("results");
+                            Log.i(TAG, results.toString());
+                            movies = Movie.parseJSON(results);
+                        } catch (JSONException e) {
+                            Log.e(TAG, "Hit JSON exception", e);
+                            e.printStackTrace();
+                        }
                     }
 
                     @Override
